3c49d4de06b100365c0f55be01930837762eaa21
Dhruv Bhanushali <dhruvbhanushali5621@gmail.com>
2021-04-18 11:47:31 +0530

    Basics of making a function

---
+++ Java - Js/Java/Functions/Basic.java
@@ -0,0 +1,16 @@
+//BASIC JAVA
+public class Main {
+  //CALL A NEW METHOD
+  static void BasicJava() {
+    //CODE INSIDE THE FUNCTION
+    System.out.println("I just created my first function");
+  }
+
+  public static void main(String[] args) {
+    //RUNNING THE FUNCTION
+    BasicJava();
+  }
+}
+
+//OUTPUT
+//I just created my first function
