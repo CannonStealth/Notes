3511ccf29f4cfd8ff037e71d61a09d9a264b1dcc
Arnav Patil <77932515+MacaroniPeppers@users.noreply.github.com>
2021-04-18 18:06:35 +0530

    Create basic.cs

---
+++ Systems/C#/basic.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Learning
+{
+    class Program
+    { //start of class
+    
+        static void Main(string[] args)
+        { //start of method
+        
+          Console.WriteLine("Hello World"); //Cursor goes to next line
+          //if Console.Write() is used, cursor will stay on same line
+          
+          Console.ReadKey(); //needed to close terminal window
+        }
+    }
+}    
