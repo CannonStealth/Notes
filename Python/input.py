51e04d4665de9a1a4cbfcb0dfeb70d6a067f8676
Dhruv Bhanushali <dhruvbhanushali5621@gmail.com>
2021-04-18 23:25:07 +0530

    Take input using python

--- Python/input.py
+++ Python/input.py
@@ -4,3 +4,10 @@ age = input("Enter your age")
 
 //PRINT THE VALUES
 print("Your name is " + name + " and your age is " + age)
+
+//INPUT
+//name = Lejhand
+//age = 69
+
+//OUTPUT
+//Your name is Lejhand and your age is 69
