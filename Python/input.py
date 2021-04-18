937bab9303c790c754d5d43b063273d0f83afe41
Dhruv Bhanushali <dhruvbhanushali5621@gmail.com>
2021-04-18 23:27:30 +0530

    Input using python

--- Python/input.py
+++ Python/input.py
@@ -1,13 +1,13 @@
-//INPUT VALUES AND STORE IN THE VARIABLE
+# INPUT VALUES AND STORE IN THE VARIABLE
 name = input("Enter your name")
 age = input("Enter your age")
 
-//PRINT THE VALUES
+# PRINT THE VALUES
 print("Your name is " + name + " and your age is " + age)
 
-//INPUT
-//name = Lejhand
-//age = 69
+# INPUT
+# name = Lejhand
+# age = 69
 
-//OUTPUT
-//Your name is Lejhand and your age is 69
+# OUTPUT
+# Your name is Lejhand and your age is 69
