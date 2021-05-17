597ec01f8059adfa79c00b65efdaa4c4a5011174
Dheeraj Prakash <dheerajpv16@gmail.com>
2021-05-17 12:58:28 -0400

    basically redid the entire JS section

--- JavaScript/Classes/class.js
+++ JavaScript/Classes/class.js
@@ -1,30 +1,63 @@
-class person {
-    constructor(hairColor = 'brown', eyeColor = 'blue') {
-        this._hairColor = hairColor
-        this._eyeColor = eyeColor
+// a class represents a TYPE of something.
+// a person is a type of thing in the universe.
+class Person {
+    // conventionally a class name uses PascalCase
+    // public field
+    // these can be directly accessed outsied the class
+    name;
+
+    // private fields
+    // cannot be accessed outside without getters or setters
+    #hairColor;
+    #eyeColor;
+
+    // a constructor is a special function that can be used with new to
+    // create an object that is an intance of this class
+    constructor(name, hairColor = "brown", eyeColor = "blue") {
+        this.name = name;
+        this.#hairColor = hairColor;
+        this.#eyeColor = eyeColor;
     }
 
+    // getters can be used to expose private fields
+    // this allows reading but not writing to the fields.
+
     get hairColor() {
-        return this._hairColor
+        return this.#hairColor;
     }
 
     get eyeColor() {
-        return this._eyeColor
+        return this.#eyeColor;
     }
 }
 
-class child extends person { // Copies all of 
-    constructor(hairColor = new person().hairColor, eyeColor = new person().eyeColor, skin = 'white') {
-        super(hairColor, eyeColor) // Super must always come first in order to be able to use the this keyword.
-        this._skin = skin
+// classes can extend, or inherit from, other classes
+// this Child class has all the properties and methods of Person
+// but also can have more, or override ones declared in Person
+class child extends Person {
+    #age = 5;
+
+    constructor(
+        name = new Person().name,
+        hairColor = new Person().hairColor,
+        eyeColor = new Person().eyeColor
+    ) {
+        // a subclass MUST use the super constructor.
+        super(name, hairColor, eyeColor);
     }
-    
-    get skin() {
-        return this._skin
+
+    ageUp() {
+        this.#age++;
+    }
+
+    get age() {
+        return this.#age;
     }
 }
 
-const cannon = new person()
-console.log(cannon.hairColor) // returns 'brown'
-const newChild = new child('blonde', undefined, 'black')
-console.log(newChild.eyeColor) // returns 'blue' as the class's eyeColor is set to the parent's default eyeColor which is blue.
+// creates a person with default parameters (except name, which is required)
+const cannon = new Person("Cannon");
+console.log(cannon.hairColor); // returns 'brown'
+
+const newChild = new child("Child", "blonde", undefined);
+console.log(newChild.eyeColor); // returns 'blue' as the class's eyeColor is set to the parent's default eyeColor which is blue.
--- JavaScript/FS AND PATH/readFile.js
+++ JavaScript/FS AND PATH/readFile.js
@@ -1,19 +1,17 @@
-const fs = require('fs')
-const path = require('path')
+// the node filesystem API will not work on the browser.
+import fs from "fs/promises";
+import path from "path";
 
 async function readFile(filePath) {
     try {
-      const data = await fs.promises.readFile(filePath); 
-      console.log(data.toString()); // MAKE SURE TO ADD .toString()
+        const data = await fs.readFile(filePath);
+        console.log(data.toString()); // MAKE SURE TO ADD .toString() as data is a Buffer
     } catch (error) {
-      console.error(`Got an error trying to read the file: ${error.message}`);
+        console.error(`Got an error trying to read the file: ${error.message}`);
     }
-  }
-readFile('greetings.js'); // we read it
+}
 
-const command = require(path.join(require.main.path, 'greetings.js')) // to get info we use require and the path
-console.log(command)
+readFile("greetings.js"); // we read the file
 
-
-
-
+const command = await import(path.join(require.main.path, "greetings.js")); // to get info we use require and the path
+console.log(command);
